7eff647bdd91f2bdd2d28b3283465670f76caaba
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2015-12-07 01:26:45 +0530

    Clean, Uglify and Sass working.

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        expand: true,
+        cwd: 'App/public/javascripts/',
+        src: '*.js',
+        dest: 'Dist/public/javascripts/',
+        ext: '.min.js',
+        extDot: 'last'
+      }
+    },
+    clean: ["Dist/*"],
+
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'App/public/stylesheets/',
+          src: ['*.scss'],
+          dest: 'Dist/public/stylesheets/',
+          ext: '.css'
+        }]
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean', 'uglify', 'sass']);
+  
+
+};
+
+
+/* use grunt clean to clean dist. */

b616a4cdba7639b84f657354ddbf0c13f726feef
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2015-12-07 01:30:14 +0530

    No generation of source file for SASS task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,10 @@ module.exports = function(grunt) {
 
     sass: {
       dist: {
+        options: { 
+          style: 'expanded',
+          sourcemap: 'none'
+        },
         files: [{
           expand: true,
           cwd: 'App/public/stylesheets/',

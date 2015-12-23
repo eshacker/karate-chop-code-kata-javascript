f6220e95e00f5943ce8fc32b38a6f7aa6230246b
Ecmascript Hacker <ecmascript.hacker@gmail.com>
2015-12-23 19:11:51 +0530

    first chop is deployed via for loop.

--- App/public/scripts/app/app-kata.js
+++ App/public/scripts/app/app-kata.js
@@ -1,5 +1,20 @@
 var firstChop = function(arr, num) {
-  
+  var i = 0, left = 0, right = arr.length, mid = 0;
+  while(left < right) {
+    mid = Math.floor((left+right)/2);
+    if(arr[mid] === num) {
+      return mid;
+    } else if(arr[mid] > num) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  if(arr[left] === num){
+    return left;
+  } else {
+    return -1;
+  }
 };
 
 var secondChop = function(arr, num) {
